6ca8526c3b85b4fb43a7e8c3b64183e586fc3a0f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-03-03 12:06:19 +0100

    can list events of a calendar

--- recurring_ical_events.py
+++ recurring_ical_events.py
@@ -1,10 +1,24 @@
+import icalendar
+
+def is_event(component):
+    """Return whether a component is a calendar event."""
+    return isinstance(component, icalendar.cal.Event)
 
 class UnfoldableCalendar:
 
+    def __init__(self, calendar):
+        assert calendar.get("CALSCALE", "GREGORIAN") == "GREGORIAN", "Only Gregorian calendars are supported." # https://www.kanzaki.com/docs/ical/calscale.html
+        self.calendar = calendar
+
     def between(self, start, end):
-        pass
+        events = []
+        for event in self.calendar.walk():
+            if not is_event(event):
+                continue
+            events.append(event)
+        return events
 
 
 def of(a_calendar):
     """Unfold recurring events of a_calendar"""
-    return UnfoldableCalendar()
+    return UnfoldableCalendar(a_calendar)
--- test/test_single_events.py
+++ test/test_single_events.py
@@ -1,6 +1,10 @@
 
 from recurring_ical_events import of
 
-def test_no_events_have_no_events(calendars):
+def test_a_calendar_with_no_events_has_no_events(calendars):
     events = of(calendars.no_events).between((2000, 1, 1), (2099,1,1))
     assert not events
+
+def test_a_calendar_with_one_event_has_one_event(calendars):
+    events = of(calendars.one_event).between((2000, 1, 1), (2099,1,1))
+    assert len(events) == 1
