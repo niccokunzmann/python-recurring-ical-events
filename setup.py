fb3db4729b4f008ccc7014e5dba839485743fbcc
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-03-03 20:23:56 +0100

    increase version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PACKAGE_NAME = "recurring_ical_events"
 HERE = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, HERE)  # for package import
 
-__version__ = "0.0.4a"
+__version__ = "0.0.5a"
 __author__ = 'Nicco Kunzmann'
 
 
