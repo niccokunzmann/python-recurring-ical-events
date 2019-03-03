dcf55e1b178eb2575d83e8178caa710986cfe16a
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-03-03 16:24:48 +0100

    add test todo

---
+++ test/test_span_overlap.py
@@ -0,0 +1,16 @@
+"""This file tests the overlap of events.
+
+It might be that events overlap the start and end of the time span.
+This must test
+- single events overlapping
+- recurrent events overlapping
+both
+- start
+- end
+with both
+- datetime
+- date
+"""
+
+def test_implement_tests(todo):
+    pass
