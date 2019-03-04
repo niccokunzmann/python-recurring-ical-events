1f852248cad57fcb710d1cf6610eb103ca3dd304
Nicco Kunzmann <niccokunzmann@rambler.ru>
2019-03-04 14:40:21 +0100

    test fixed

--- test/test_germany.py
+++ test/test_germany.py
@@ -2,4 +2,4 @@
 def test_date_events_can_be_used(calendars):
     events = calendars.Germany.between("20140511T000000Z", "20140511T000000Z")
     assert len(events) == 1
-    assert events["SUMMARY"] == "Germany: Mother's Day [Not a public holiday]"
+    assert events[0]["SUMMARY"] == "Germany: Mother's Day [Not a public holiday]"
