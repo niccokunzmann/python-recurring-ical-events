8dddd22274fa83fd53cd21a11f9bfe9ce40198ac
Anton Shkurenko <anton.shkurenko@outlook.com>
2019-10-09 09:54:07 +0200

    Update at function tests

--- test/test_at_function.py
+++ test/test_at_function.py
@@ -1,16 +1,33 @@
 import pytest
 from datetime import date, datetime
+from icalendar import Event
 
 
-@pytest.mark.parametrize("a_date", [
+@pytest.mark.parametrize("a_date,count", [
     # Year
-    2019, (2019,),
+    (2018, 3), ((2018,), 3),
     # Month
-    (2019, 1),
+    ((2018, 1), 3),
     # Day
-    (2019, 1, 1), date(2019, 1, 1), "20190101",
+    ((2018, 1, 11), 1), (date(2018, 1, 11), 1), ("20180111", 1),
     # Datetime
-    datetime(2019, 1, 1, 10, 0, 0)
+    (datetime(2018, 1, 11, 10, 0, 0), 0)
 ])
-def test_at_input_arguments(a_date, calendars):
-    calendars.duration.at(a_date)
+def test_at_input_arguments(a_date, count, calendars):
+    events = calendars.duration.at(a_date)
+    assert isinstance(events, list)
+    for event in events:
+        assert isinstance(event, Event)
+    assert len(events) == count
+
+
+@pytest.mark.parametrize("a_date,count", [
+    (datetime(2018, 1, 15, 9, 59, 0), 0),
+    (datetime(2018, 1, 15, 10, 0, 0), 1),
+    (datetime(2018, 1, 15, 11, 0, 0), 1),
+    (datetime(2018, 1, 15, 13, 0, 0), 0),
+    (datetime(2018, 1, 15, 13, 1, 0), 0),
+])
+def test_at_datetime_input(a_date, count, calendars):
+    events = calendars.duration.at(a_date)
+    assert len(events) == count
