e8d147e8641537f1dc89cdf1c3d46e548f6eaa93
Anton Shkurenko <anton.shkurenko@outlook.com>
2019-10-09 11:32:45 +0200

    Remove test for datetime and output type checking

--- test/test_at_function.py
+++ test/test_at_function.py
@@ -1,6 +1,5 @@
 import pytest
 from datetime import date, datetime
-from icalendar import Event
 
 
 @pytest.mark.parametrize("a_date,count", [
@@ -14,20 +13,5 @@ from icalendar import Event
     (datetime(2018, 1, 11, 10, 0, 0), 0)
 ])
 def test_at_input_arguments(a_date, count, calendars):
-    events = calendars.duration.at(a_date)
-    assert isinstance(events, list)
-    for event in events:
-        assert isinstance(event, Event)
-    assert len(events) == count
-
-
-@pytest.mark.parametrize("a_date,count", [
-    (datetime(2018, 1, 15, 9, 59, 0), 0),
-    (datetime(2018, 1, 15, 10, 0, 0), 1),
-    (datetime(2018, 1, 15, 11, 0, 0), 1),
-    (datetime(2018, 1, 15, 13, 0, 0), 0),
-    (datetime(2018, 1, 15, 13, 1, 0), 0),
-])
-def test_at_datetime_input(a_date, count, calendars):
     events = calendars.duration.at(a_date)
     assert len(events) == count
