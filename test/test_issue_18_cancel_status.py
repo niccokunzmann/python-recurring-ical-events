9b301a423d5014b7c64d76ec4a051d71fb740377
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-02-06 22:16:42 +0100

    test cancel status of events
    - fixes https://github.com/niccokunzmann/python-recurring-ical-events/issues/18

---
+++ test/test_issue_18_cancel_status.py
@@ -0,0 +1,15 @@
+"""Test that cancelled events are actually repeated as cancelled."""
+import pytest
+
+@pytest.mark.parametrize("date,attr,value", [
+    ("20200128", "STATUS", None),
+    ("20200129", "STATUS", "CANCELLED"),
+    ("20200130", "STATUS", None),
+    ("20200128", "TRANSP", "OPAQUE"),
+    ("20200129", "TRANSP", "OPAQUE"),
+    ("20200130", "TRANSP", "OPAQUE"),
+])
+def test_events_are_cancelles(calendars, date, attr, value):
+    event = calendars.issue_18_cancel_status.at(date)[0]
+    assert event.get(attr) == value
+
