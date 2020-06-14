9264cae4f452a88fe216115d95575662e29574e1
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-06-14 21:50:35 +0200

    test for right amount of events
    see https://github.com/niccokunzmann/python-recurring-ical-events/issues/28#issuecomment-643805419

--- test/test_issue_28_timezone_with_z.py
+++ test/test_issue_28_timezone_with_z.py
@@ -6,5 +6,5 @@ import pytest
 
 def test_expected_amount_of_events(calendars):
     events = calendars.issue_28_rrule_with_UTC_endinginZ.between((2020, 5, 25),(2020, 9, 5))
-    assert len(events) == 15
+    assert len(events) == 17
 
