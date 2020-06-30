4e421963ec954e6eaa712fceca1e84968d043390
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-06-30 21:04:09 +0200

    document the test better

--- test/test_issue_27.py
+++ test/test_issue_27.py
@@ -1,6 +1,16 @@
 """These tests are for Issue 27
 https://github.com/niccokunzmann/python-recurring-ical-events/issues/27
 
+Diff of the two files:
+
+diff test/calendars/issue-27-t1.ics test/calendars/issue-27-t2.ics
+
+    < RRULE:FREQ=DAILY;UNTIL=20200429T000000
+    < EXDATE:20200427T120000Z
+    ---
+    > RRULE:FREQ=DAILY;UNTIL=20200429T000000Z
+    > EXDATE:20200427T140000Z
+
 """
 
 start_date = (2020, 4, 25)
@@ -13,14 +23,14 @@ def print_events(events):
         print("start {} duration {}".format(start, duration))
 
 
-def test_until_value_with_unknown_timezone_works_with_exdate(calendars):
+def test_until_value_with_UNKNOWN_timezone_works_with_exdate(calendars):
     """The until value has no time zone attached."""
     events = calendars.issue_27_t1.between(start_date, end_date)
     print_events(events)
     assert len(events) == 2, "two events, exdate matches one"
 
 
-def test_until_value_with_default_timezone_works_with_exdate(calendars):
+def test_until_value_with_DEFAULT_timezone_works_with_exdate(calendars):
     """The until value uses the default time zone."""
     events = calendars.issue_27_t2.between(start_date, end_date)
     print_events(events)
