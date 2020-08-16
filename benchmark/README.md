803e54ddcf7dba3a0c7be0b7ce76cd8b387640ab
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-08-17 00:42:12 +0100

    create benchmark folder

---
+++ benchmark/README.md
@@ -0,0 +1,7 @@
+# speed tests
+
+This folder contains speed tests. Run them from within the root folder (next to the module).
+
+```
+time python3 benchmark/issue42.py
+```
--- benchmark/issue42.py
+++ benchmark/issue42.py
@@ -1,12 +1,21 @@
 #py3
+#
+# This is the benchmark for rrule speed
+# see https://github.com/niccokunzmann/python-recurring-ical-events/issues/42
+#
 
 import icalendar
+import os
+import sys
+
+HERE = os.path.dirname(__file__) or "."
+sys.path.append(os.path.join(HERE, ".."))
+
 import recurring_ical_events
-import urllib.request
 
 # read utf
 text = []
-with open('dummy.ics', "rb") as fobj:
+with open(os.path.join(HERE, 'issue42.ics'), "rb") as fobj:
   text += fobj.readlines()
 text_utf = []
 for i in text:
