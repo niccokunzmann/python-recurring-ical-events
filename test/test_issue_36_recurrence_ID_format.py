e352bc55f7ecfbef75f32e5e64fda4ac451fba68
Daniel Stolpmann <dstolpmann-dev@online.de>
2020-09-22 17:41:11 +0200

    Add tests for issue #36

---
+++ test/test_issue_36_recurrence_ID_format.py
@@ -0,0 +1,19 @@
+'''
+These tests are for issue 36
+https://github.com/niccokunzmann/python-recurring-ical-events/issues/36
+'''
+
+def test_datetime_replaced_by_datetime(calendars):
+    events = calendars.issue_36_recurrence_ID_format.at("20200917")
+    assert len(events) == 1
+    assert events[0].get("SUMMARY") == "Modified event"
+
+def test_date_replaced_by_date(calendars):
+    events = calendars.issue_36_recurrence_ID_format.at("20200914")
+    assert len(events) == 1
+    assert events[0].get("SUMMARY") == "Modified event 1"
+
+def test_date_replaced_by_datetime(calendars):
+    events = calendars.issue_36_recurrence_ID_format.at("20200921")
+    assert len(events) == 1
+    assert events[0].get("SUMMARY") == "Modified event 2"
