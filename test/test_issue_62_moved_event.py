7597dd0e865c5ffec96b0dfe65a96ac12eda57ac
Nicco Kunzmann <niccokunzmann@rambler.ru>
2021-12-23 11:56:56 +0000

    tests showing that the event is not moved
    see https://github.com/niccokunzmann/python-recurring-ical-events/issues/62

---
+++ test/test_issue_62_moved_event.py
@@ -0,0 +1,19 @@
+'''
+This tests the move of a december event.
+
+Issue: https://github.com/niccokunzmann/python-recurring-ical-events/issues/62
+'''
+
+def test_event_is_absent(calendars):
+    '''RRULE:FREQ=MONTHLY;BYDAY=-1FR'''
+    events = calendars.issue_62_moved_event.at("20211231")
+    assert events == []
+
+def test_event_has_moved(calendars):
+    '''DTSTART;TZID=Europe/Berlin:20211217T213000'''
+    events = calendars.issue_62_moved_event.at("20211217")
+    assert len(events) == 1
+    
+    
+
+
