7070c688b046ff6d20af4ba8671dfe46e658a6e8
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-01-05 15:13:52 +0000

    add tests for input of between

--- test/test_convert_inputs.py
+++ test/test_convert_inputs.py
@@ -1,13 +1,9 @@
+'''Test that different inputs are understood'''
+import pytest
 
-
-def test_calendar_between_allows_tuple(todo):
-    pass
-
-def test_calendar_between_allows_float(todo):
-    pass
-
-def test_calendar_between_allows_date(todo):
-    pass
-
-def test_calendar_between_allows_datetime(todo):
-    pass
+@pytest.mark.parametrize("start,stop,event_count", [
+  (2020, 2021, None)
+])
+def test_calendar_between_allows_tuple(calendars, start, stop, event_count):
+    events = calendars.one_day_event_repeat_every_day.between(start, stop)
+    assert len(events) == event_count
