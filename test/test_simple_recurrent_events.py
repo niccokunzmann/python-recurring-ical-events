b9bad8af723a8093e88dc41eaa95575c145e6d49
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-01-07 20:26:08 +0000

    test that different time zones as arguments actually make a differene in the retrieved events

--- test/test_simple_recurrent_events.py
+++ test/test_simple_recurrent_events.py
@@ -21,13 +21,6 @@ def test_events_occur_after_and_before_span_end(calendars, count, end):
     events = calendars.three_events.between((2000, 1, 1), end)
     assert len(events) == count
 
-def test_include_events_if_the_time_zone_differs(todo):
-    """When the time zone is different, events can be included or
-    excluded because they are in another time zone.
-    This may only be a problem for tests though because users will
-    probably use time zones if it really makes a difference.
-    """
-
 @pytest.mark.parametrize("count,start",[
     (3, (2019, 3, 3)),
     (2, (2019, 3, 5)),
