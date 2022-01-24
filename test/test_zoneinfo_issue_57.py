052705a05f9a13a2b230c3cb841ece9339d15e51
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-01-24 22:02:32 +0000

    test installation of modules is complete

--- test/test_zoneinfo_issue_57.py
+++ test/test_zoneinfo_issue_57.py
@@ -6,6 +6,8 @@ from datetime import date, datetime, timedelta
 
 from icalendar import Calendar, Event, vDDDTypes
 import recurring_ical_events
+import sys
+from importlib.util import find_spec as module_exists
 
 
 def test_zoneinfo_example_yields_events(ZoneInfo):
@@ -28,3 +30,15 @@ def test_zoneinfo_example_yields_events(ZoneInfo):
     events = recurring_ical_events.of(cal).between(d, d + timedelta(1))
 
     assert len(events) == 1, "The event was found."
+
+
+def test_zoneinfo_must_be_installed_if_it_is_possible():
+    """Make sure that zoneinfo and tzdata are installed if possible."""
+    python_version = sys.version_info[:2]
+    if python_version < (3, 7):
+        return # no zoneinfo
+    if python_version <= (3, 8):
+        assert module_exists("backports.zoneinfo"), "zoneinfo should be installed with pip install backports.zoneinfo"
+    else:
+        assert module_exists("zoneinfo"), "We assume that zoneinfo exists."
+    assert module_exists("tzdata"), "tzdata is necessary to test current time zone understanding."
