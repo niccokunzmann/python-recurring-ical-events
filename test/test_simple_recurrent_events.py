58aedd0eac1757be572433e1e609bac58f7e7517
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-02-01 15:03:09 +0000

    fix maximum datetime in tests

--- test/test_simple_recurrent_events.py
+++ test/test_simple_recurrent_events.py
@@ -1,15 +1,17 @@
 import pytest
+from recurring_ical_events import DATE_MAX
+
 
 def test_event_is_not_included_if_it_is_later(calendars):
     events = calendars.three_events.between((2000, 1, 1), (2001,1,1))
     assert not events
 
 def test_event_is_not_included_if_it_is_earlier(calendars):
-    events = calendars.three_events.between((2099, 1, 1), (2100,1,1))
+    events = calendars.three_events.between((2030, 1, 1), DATE_MAX)
     assert not events
 
 def test_all_events_in_time_span(calendars):
-    events = calendars.three_events.between((2000, 1, 1), (2100,1,1))
+    events = calendars.three_events.between((2000, 1, 1), DATE_MAX)
     assert len(events) == 3
 
 @pytest.mark.parametrize("count,end",[
@@ -27,5 +29,5 @@ def test_events_occur_after_and_before_span_end(calendars, count, end):
     (1, (2019, 3, 8)),
 ])
 def test_events_occur_after_and_before_span_start(calendars, count, start):
-    events = calendars.three_events.between(start, (2100, 1, 1))
+    events = calendars.three_events.between(start, DATE_MAX)
     assert len(events) == count
