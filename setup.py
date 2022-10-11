a32a6f7dd4a4ca6745e13b872b6619c578ea3040
Nicco Kunzmann <niccokunzmann@rambler.ru>
2022-10-11 13:14:33 +0100

    v1.0.3b

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PACKAGE_NAME = "recurring_ical_events"
 HERE = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, HERE)  # for package import
 
-__version__ = "1.0.2b"
+__version__ = "1.0.3b"
 __author__ = 'Nicco Kunzmann'
 
 
