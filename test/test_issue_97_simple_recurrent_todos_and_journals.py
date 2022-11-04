19e7568fd35262c65ce5aacbae0bd7328bc4923b
Tobias Brox <tobias@redpill-linpro.com>
2022-11-04 13:02:11 +0100

    clearing a warning

--- test/test_issue_97_simple_recurrent_todos_and_journals.py
+++ test/test_issue_97_simple_recurrent_todos_and_journals.py
@@ -1,13 +1,13 @@
 import pytest
 from recurring_ical_events import DATE_MAX
 
-testdata_parametrized = pytest.mark.parametrize("ical_file", [
+calendars_parametrized = pytest.mark.parametrize("ical_file", [
     "issue_97_simple_todo",
     "issue_97_simple_journal",
     "issue_97_todo_nodtstart",
 ])
 
-@testdata_parametrized
+@calendars_parametrized
 def test_recurring_task_is_not_included1(calendars, ical_file):
     """The three files given starts in late 1991, no recurrences
     should be found before 1991.  Refers to
@@ -19,7 +19,7 @@ def test_recurring_task_is_not_included1(calendars, ical_file):
     tasks = calendar.between((1989, 1, 1), (1991,1,1))
     assert not tasks
 
-@testdata_parametrized
+@calendars_parametrized
 def test_recurring_task_is_not_included2(calendars, ical_file):
     """Every recurrence of the three ical files is in October, hence
     no recurrences should be found.  Refers to
@@ -31,7 +31,7 @@ def test_recurring_task_is_not_included2(calendars, ical_file):
     tasks = calendar.between((1998, 1, 1), (1998,4,14))
     assert not tasks
 
-@testdata_parametrized
+@calendars_parametrized
 def test_recurring_task_is_repeated(calendars, ical_file):
     """Expansion of a yearly task over seven years.
     The issue
