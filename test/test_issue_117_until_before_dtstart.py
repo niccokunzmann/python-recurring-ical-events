1d8376da05aa8f593e7e452d03f3ca07d7ee8991
Nicco Kunzmann <niccokunzmann@rambler.ru>
2023-11-19 22:45:17 +0000

    reproduce error of issue 117

---
+++ test/test_issue_117_until_before_dtstart.py
@@ -0,0 +1,8 @@
+"""The atlassian confluence calendar sets the until value lower than the DTSTART when then event is deleted.
+
+See https://github.com/niccokunzmann/python-recurring-ical-events/issues/117
+"""
+
+def test_event_is_deleted(calendars):
+    """No event takes place."""
+    assert not calendars.issue_117_until_before_dtstart.all()
