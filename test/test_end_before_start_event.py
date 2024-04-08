0129c38471c4cf77d09143164524a8c311808abc
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-04-08 22:59:10 +0100

    format all files with ruff
    pre-commit run --all-files

--- test/test_end_before_start_event.py
+++ test/test_end_before_start_event.py
@@ -1,8 +1,8 @@
 import pytest
+
 from recurring_ical_events import PeriodEndBeforeStart
 
 
 def test_end_before_start_event(calendars):
     with pytest.raises(PeriodEndBeforeStart):
         calendars.end_before_start_event.at(2019)
-
