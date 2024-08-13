5786b887664023f0e0bf17a1fbcbfdc1a160b0a0
Nicco Kunzmann <niccokunzmann@rambler.ru>
2024-08-14 00:48:28 +0100

    correct test case

--- test/test_issue_151_macos_linux_difference.py
+++ test/test_issue_151_macos_linux_difference.py
@@ -7,11 +7,11 @@ from datetime import datetime, timezone
 
 
 def test_count_events_from_issue(calendars):
-    """The number of events seems to differ from OS to OS."""
+    """Avents were omitted through version upgrade from 2.2.2 to 2.2.3."""
 
     start_time = datetime.fromtimestamp(1722564000, timezone.utc)
     end_time = datetime.fromtimestamp(1722567600, timezone.utc)
     events = calendars.issue_151_macos_linux_difference.between(start_time, end_time)
     for event in events:
         print(event["UID"], event["DTSTART"], event["SUMMARY"])
-    assert len(events) == 4
+    assert len(events) == 1
