b1ce8b7ecbb30af862595651ab4d15545a013cc3
Jochen Sprickerhof <jspricke@debian.org>
2024-11-01 17:15:32 +0100

    Use non legacy timezone name in test

--- test/test_time_zones_differ.py
+++ test/test_time_zones_differ.py
@@ -12,7 +12,7 @@ import pytz
         # DTSTART;TZID=Europe/Berlin:20190304T000000
         # time zone offset 6:07:00 between Europe/Berlin and Asia/Ho_Chi_Minh
         ((2019, 3, 4), 24, "Europe/Berlin", 1, "three_events"),
-        ((2019, 3, 4), 24, "EST", 0, "three_events"),
+        ((2019, 3, 4), 24, "America/Panama", 0, "three_events"),
         ((2019, 3, 4), 24, "Asia/Ho_Chi_Minh", 1, "three_events"),
         ((2019, 3, 4), 1, "Asia/Ho_Chi_Minh", 0, "three_events"),
         ((2019, 3, 4), 6, "Asia/Ho_Chi_Minh", 0, "three_events"),
@@ -20,7 +20,7 @@ import pytz
         # events that have no time zone, New Year
         ((2019, 1, 1), 1, "Europe/Berlin", 1, "Germany"),
         ((2019, 1, 1), 1, "Asia/Ho_Chi_Minh", 1, "Germany"),
-        ((2019, 1, 1), 1, "EST", 1, "Germany"),
+        ((2019, 1, 1), 1, "America/Panama", 1, "Germany"),
     ],
 )
 def test_include_events_if_the_time_zone_differs(
