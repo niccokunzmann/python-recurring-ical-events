5a88bc0b05c3e87df9b012b6a1f9efaa71da167d
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-02-10 15:10:10 +0000

    Reomve Python 3.8 errors

--- recurring_ical_events/types.py
+++ recurring_ical_events/types.py
@@ -3,15 +3,15 @@
 from __future__ import annotations
 
 import datetime
-from typing import Union
+from typing import Tuple, Union
 
 Time = Union[datetime.date, datetime.datetime]
-DateArgument = Union[tuple[int], datetime.date, str, int]
+DateArgument = Union[Tuple[int], datetime.date, str, int]
 UID = str
-ComponentID = tuple[str, UID, Time]
+ComponentID = Tuple[str, UID, Time]
 Timestamp = float
 RecurrenceID = datetime.datetime
-RecurrenceIDs = tuple[RecurrenceID]
+RecurrenceIDs = Tuple[RecurrenceID]
 
 __all__ = [
     "Time",
