50abcba151a7ece89db2a8556d2ec673e62d20d9
Nicco Kunzmann <niccokunzmann@rambler.ru>
2025-05-19 13:08:21 +0100

    add docs build with sphinx

---
+++ docs/conf.py
@@ -0,0 +1,92 @@
+# icalendar documentation build configuration file
+import importlib.metadata
+import datetime
+import os
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx_copybutton",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
+]
+source_suffix = {".rst": "restructuredtext"}
+master_doc = "index"
+
+project = "recurring-ical-events"
+this_year = datetime.date.today().year
+copyright = f"{this_year}, Nicco Kunzmann & recurring-ical-events contributors"
+release = version = importlib.metadata.version("recurring-ical-events")
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/niccokunzmann/python-recurring-ical-events",
+            "icon": "fa-brands fa-square-github",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css"
+            }
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/recurring-ical-events",
+            "icon": "fa-solid fa-download",
+            "type": "fontawesome",
+            "attributes": {
+                "target": "_blank",
+                "rel": "noopener me",
+                "class": "nav-link custom-fancy-css"
+            }
+        },
+    ],
+    "navigation_with_keys": True,
+    "search_bar_text": "Search",
+    "show_nav_level": 2,
+    "show_toc_level": 2,
+    "use_edit_page_button": True,
+}
+html_context = {
+#     "github_url": "https://github.com", # or your GitHub Enterprise site
+    "github_user": "niccokunzmann",
+    "github_repo": "python-recurring-ical-events",
+    "github_version": "main",
+    "doc_path": "docs",
+}
+htmlhelp_basename = "recurring-ical-events-doc"
+pygments_style = "sphinx"
+
+
+# -- Intersphinx configuration ----------------------------------
+
+# This extension can generate automatic links to the documentation of objects
+# in other projects. Usage is simple: whenever Sphinx encounters a
+# cross-reference that has no matching target in the current documentation set,
+# it looks for targets in the documentation sets configured in
+# intersphinx_mapping. A reference like :py:class:`zipfile.ZipFile` can then
+# linkto the Python documentation for the ZipFile class, without you having to
+# specify where it is located exactly.
+#
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "icalendar": ("https://icalendar.readthedocs.io/en/latest", None),
+}
+
+
+man_pages = [("index", "recurring_ical_events", "recurring_ical_events Documentation", ["Nicco Kunzmann"], 1)]
+
+exclude_patterns = [
+    "venv"
+]
